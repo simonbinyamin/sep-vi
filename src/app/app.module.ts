db7d91becaef869f56518ce799344a3f043aff17
simon <simon.m.binyamin@gmail.com>
2020-10-03 18:09:25 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot([
+        { path: '', component: LoginComponent, pathMatch: 'full' },
+        { path: 'Login', component: LoginComponent},
+
+    ])
+],
   providers: [],
   bootstrap: [AppComponent]
 })
